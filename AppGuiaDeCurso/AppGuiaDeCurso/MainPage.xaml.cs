f79c4ca4c080c65d47b2d5404352483c2eb8c86f
Usuario <Usuario@CPD03-M09>
2022-09-28 15:53:48 -0300

    Adicionar arquivos de projeto.

---
+++ AppGuiaDeCurso/AppGuiaDeCurso/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using AppGuiaDeCurso.Views;
+
+namespace AppGuiaDeCurso
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            img.Source = ImageSource.FromResource("AppGuiaDeCurso.img.logo.png");
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+        }
+
+        private void BtnInicial_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPrimeiro_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSegundo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnTerceiro_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnVestibulinho_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AppGuiaDeCurso/AppGuiaDeCurso/Models/Componentes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGuiaDeCurso.Models
+{
+    internal class Componentes
+    {
+        public string Nome { get; set; }
+
+        public int CargaHoraria { get; set; }
+
+        public string AtribuicoesResponsabilidades { get; set; }
+
+        public string ValoresAtitudes { get; set; }
+
+        public string Competencias { get; set; }
+
+        public string Habilidades { get; set; }
+
+        public string BasesTecnologicas { get; set; }
+    }
+}
